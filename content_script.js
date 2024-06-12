3113ffacdbf2822b0996ac52ab624dbdcc139f6f
Shigenori Suzuki <s.suzuki@gmail.com>
2024-06-12 10:07:55 +0900

    modify getBookInfo func

--- content_script.js
+++ content_script.js
@@ -10,10 +10,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   function getBookInfo() {
     const title = document.getElementById("productTitle")?.innerText.trim();
     const author = Array.from(document.getElementsByClassName("author notFaded")).map(el => el.innerText.trim()).join(", ");
-    const publisher = document.getElementById("rpi-attribute-book_details-publisher")?.innerText.replace(/(?:^|\s)(Publisher|出版社)(?::|\s)\s*/i, '').trim();
-    const publishedDate = document.getElementById("rpi-attribute-book_details-publication_date")?.innerText.replace(/(?:^|\s)(Published|発売日)(?::|\s)\s*/i, '').trim();
+    const publisher = document.getElementById("rpi-attribute-book_details-publisher")?.textContent.replace(/(?:^|\s)(Publisher|出版社)(?::|\s)\s*/i, '').trim();
+    const publishedDate = document.getElementById("rpi-attribute-book_details-publication_date")?.textContent.replace(/(?:^|\s)(Published|発売日)(?::|\s)\s*/i, '').trim();
     const imageUrl = document.getElementById("landingImage")?.src;
     const productUrl = window.location.href.split(/[?#]/)[0]; 
 
     return { title, author, publisher, publishedDate, imageUrl, productUrl };
   }
+
+  // console.log(getBookInfo());
